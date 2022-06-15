5c93ec083b7f870050c969e7ffe577116586db95
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-06-15 13:27:28 +0300

    Complete first exercise

---
+++ index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+mongoose. connect('mongodb://localhost/mongo-exercises')
+    .then(() => console.log('Connected to database...'))
+    .catch(err => console.error('Error connecting to database: ', err))
+
+// Schema: Class template
+const coursesSchema = mongoose.Schema({
+    tags: [ String ], 
+    date: { type: Date, default: Date.now }, 
+    name: String,
+    author: String,
+    isPublished: Boolean,
+    price: Number
+})
+
+// model: Class, bundled
+const Course = mongoose.model('Course', coursesSchema)
+
+async function displayCourses() {
+    try {
+        return await Course
+                        .find({ isPublished: true, tags: 'backend' })
+                        .sort({ name: 1 })
+                        .select({ name: 1, author: 1 })
+    } catch (error) {
+        console.error('Error fetching courses: ', error)
+    }
+}
+
+async function run () {
+    const result = await displayCourses()
+    console.log(result)
+}
+
+run()
+
